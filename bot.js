c60cc09e253c12ff687d9c4b98a46233eec4004a
Tony-Stark7870 <64720425+Tony-Stark7870@users.noreply.github.com>
2020-05-03 12:59:57 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,25 +10,25 @@ client.on('message', msg => {
     msg.reply('I dont want to help you, you help yourself.');
   }else if (msg.content === '.code' ) {
     msg.reply('Stop asking for the code, you douche. Codes are announced at #announcements.')
-} else if (msg.content === '.ping' ) {
+}else if (msg.content === '.ping' ) {
     msg.reply('Im not replying with pong. Nice try.')
-} else if (msg.content === '.kick' ) {
+}else if (msg.content === '.kick' ) {
     msg.reply('You dont have the permission to do that lmao.')
-} else if (msg.content === '.ban' ) {
+}else if (msg.content === '.ban' ) {
     msg.reply('You dont have the permission to do that lmao.')
-} else if (msg.content === '.robux' ) {
+}else if (msg.content === '.robux' ) {
     msg.reply('Why tf are you asking me? Youre literally at a server based on a website that gives you free robux.')
-} else if (msg.content === '.pp' ) {
+}else if (msg.content === '.pp' ) {
     msg.reply('8D. Thats short. Im not amazed.')
-} else if (msg.content === '.pp' ) {
+}else if (msg.content === '.pp' ) {
     msg.reply('8==D.Thats short. Im not amazed.')
-} else if (msg.content === '.pp' ) {
+}else if (msg.content === '.pp' ) {
     msg.reply('8=D. Thats short. Im not amazed.')
 }else if (msg.content === '.hi' ) {
     msg.reply('Seriously. Not even a hello? Just a short hi. Thats basic.')
-} else if (msg.content === '.hello' ) {
+}else if (msg.content === '.hello' ) {
     msg.reply('Its good that you said hello and not hi, but thats still weak.')
-} else if (msg.content === 'hey' ) {
+}else if (msg.content === 'hey' ) {
     msg.reply('A man has fallen into the river in discord city!')
 }
 });
